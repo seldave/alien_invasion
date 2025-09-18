a0c64d9bb3529a26cc8f34b008a06c364ebf1d31
Selassie Davids <seladavids90@gmail.com>
2025-09-18 17:59:50 +0000

    limiting the bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -27,7 +27,7 @@ class AlienInvasion:
             for bullet in self.bullets.copy():
                 if bullet.rect.bottom <= 0:
                     self.bullets.remove(bullet)
-    
+
             self._update_screen()
             self.clock.tick(60)
 
@@ -65,8 +65,9 @@ class AlienInvasion:
 
     def _fire_bullet(self):
         """ Create a new bullet and add it to the bullets group"""
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen"""
